0a9295d4c0c2591a7a4c09c74e48c9e7247ded8f
Sukpun <tabakoff.sd@yandex.ru>
2025-04-26 21:06:23 +0500

    серега симс

--- player.gd
+++ player.gd
@@ -1,31 +1,35 @@
 extends CharacterBody2D
 
-@export var speed := 100
-@onready var anim := $AnimatedSprite2D
+const SPEED = 200
+
+
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
-	var dir = Vector2.ZERO
+	var direction = Vector2.ZERO
 
 	if Input.is_action_pressed("ui_right"):
-		dir.x += 1
-	elif Input.is_action_pressed("ui_left"):
-		dir.x -= 1
-	elif Input.is_action_pressed("ui_down"):
-		dir.y += 1
-	elif Input.is_action_pressed("ui_up"):
-		dir.y -= 1
+		direction.x += 1
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		direction.y += 1
+	if Input.is_action_pressed("ui_up"):
+		direction.y -= 1
 
-	# Воспроизведение анимации по направлению
-	if dir.x > 0:
-		anim.play("walk_right")
-	elif dir.x < 0:
-		anim.play("walk_left")
-	elif dir.y > 0:
-		anim.play("walk_down")
-	elif dir.y < 0:
-		anim.play("walk_up")
-	else:
-		anim.stop()
+	direction = direction.normalized()
 
-	velocity = dir.normalized() * speed
+	velocity = direction * SPEED
 	move_and_slide()
+
+	# Смена анимации по направлению
+	if direction.x > 0:
+		animated_sprite.play("walk_right")
+	elif direction.x < 0:
+		animated_sprite.play("walk_left")
+	elif direction.y > 0:
+		animated_sprite.play("walk_down")
+	elif direction.y < 0:
+		animated_sprite.play("walk_up") # Можешь отдельно сделать "walk_up", если есть кадры
+	else:
+		animated_sprite.stop() # Стоит — стопаем анимацию
